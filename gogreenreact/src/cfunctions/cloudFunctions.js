2dea2bed24778e48b73d05203b7fffc0aff1b41f
Brian Gitau <briangitau02@gmail.com>
2023-10-20 08:50:53 +0300

    first commit

---
+++ gogreenreact/src/cfunctions/cloudFunctions.js
@@ -0,0 +1,47 @@
+export const cloudFunctionsUrls = [
+    {
+      name: "getPie",
+      url:""
+    },
+    {
+    name: "getBar",
+    url:""
+    },
+    {
+    name: "getFeed",
+    url:"https://example.com"
+    },
+    {
+    name: "postFeed",
+    url:""
+    },
+    {
+    name: "getSensorData",
+    url:""
+    },
+    
+    {
+    name: "getProgressTracker",
+    url:""
+    },
+    {
+    name: "getCostEstimate",
+    url:""
+    },
+
+    {
+    name: "geLSTMPrediction",
+    url:""
+    },
+
+    {
+    name: "getLand",
+    url:""
+    },
+
+    {
+        name: "postLease",
+        url:""
+    },
+
+]
